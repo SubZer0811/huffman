1f581ba91b680f76ed0785b603285cd85ee8848e
Subash Mylraj <subashmylraj@gmail.com>
2021-03-25 23:54:57 +0300

    Added documentation to huffman_c
    Cleaned code and added documentation (using doxygen).

--- src/huffman_c.cpp
+++ src/huffman_c.cpp
@@ -5,16 +5,19 @@
 
 using namespace std;
 
+/**
+ * @brief Generates and encoding table for a given file
+ */
 void huffman_c::generate_encoding (){
 
 	ifstream file;
 	file.open(uncomp_file);
 	char c;
 	
-	int count=0, highest=0;				//highest is the max value of count.
-	map<char, int> pre_encoding_table;
+	int count=0, highest=0;					// highest is the max value of count.
+	map<char, int> pre_encoding_table;		// create a mao between character and its count.
 
-	while((file.get(c))){
+	while((file.get(c))){					// read each character in file and append to map.
 		
 		// printf("%c", c);
 		if(pre_encoding_table.find(c) == pre_encoding_table.end()){
@@ -26,22 +29,31 @@ void huffman_c::generate_encoding (){
 		}
 	}
 	
-	int bits = ceil(log2(count)) + 1;
+	int bits = ceil(log2(count)) + 1;		// since this is constant constant huffman coding,
+											// number of bits in each encoding needs to be constant.
 
+	// assign an encoding to each character and append to encoding table
 	for(auto &x : pre_encoding_table){
 		// cout<<x.first<<": "<<x.second<<endl;
 		string code;
-		code = convertToBinary(count, bits);
+		code = convertToBinary(count, bits);	
 		count -= 1;
-		// // cout<<code<<endl;
+		// cout<<code<<endl;
 		encoding_table[x.first] = code;
 	}
 
-	// show_encoding();
+	show_encoding();
 
 	file.close();
 }
 
+/**
+ * @brief Converts an unsigned integer 'n' to a 'bits' length binary number
+ * 
+ * @param n Unsigned integer to convert
+ * @param bits Length of binary number
+ * @return string 'bits' long Binary number
+ */
 string huffman_c::convertToBinary(unsigned int n, int bits)
 {	
 	string code;
@@ -58,17 +70,27 @@ string huffman_c::convertToBinary(unsigned int n, int bits)
 	return code;
 }
 
+/**
+ * @brief Print the encoding table to STDOUT
+ */
 void huffman_c::show_encoding (){
 	for(auto &x : encoding_table){
 		cout<<x.first<<": "<<x.second<<endl;
 	}
 }
 
+/**
+ * @brief Compress the input file using constant length Huffman encoding.
+ */
 void huffman_c::save_compressed_file (){
 
 	ofstream file;
 	file.open(comp_file, ios::out | ios::binary);
 
+	/* 
+	 * Write the Encoding table to the output file. 
+	 * Each encoding entry is delimited by a space.
+	*/
 	for(auto& x : encoding_table){
 		char enc = 0;
 		string towrite;
@@ -79,7 +101,7 @@ void huffman_c::save_compressed_file (){
 		file<<x.second<<towrite;
 	}
 
-	file<<"#";
+	file<<"#"; 					// The encoding table is delimited with the compressed text by a '#'
 
 	ifstream in_file;
 	in_file.open(uncomp_file, ios::binary);
@@ -88,7 +110,11 @@ void huffman_c::save_compressed_file (){
 	int count = 0, rem = 0;
 	string enc;
 
-	while(in_file.get(c)){
+	/* 
+	 * Read each character in the input file, append its encoding to `writeto`.
+	 * If the length of `writeto` is 1 byte, then write it to the compressed file.
+	*/
+	while(in_file.get(c)){		
 
 		enc = encoding_table[c];
 		// cout<<enc<<" ";
@@ -122,6 +148,9 @@ void huffman_c::save_compressed_file (){
 	}
 	// printf("\npost null: %x \n", writeto);
 	// printf("\ncount = %d", count);
+	/**
+	 * Write all the left over bits in `writeto` if there exists any.
+	 */
 	if(count){
 		while(count < 8){
 			writeto <<= 1;
@@ -136,18 +165,22 @@ void huffman_c::save_compressed_file (){
 	file.close();
 }
 
+/**
+ * @brief Uncompress the constant length huffman compressed file.
+ */
 void huffman_c::uncompress_file (){
 	
 	ifstream file;
 	file.open(comp_file, ios::binary);
-
-	string msg;
 
 	char c;
 	map<string, char> encoding_table;
 
 	file.get(c);
 
+	/**
+	 * Extract the encoding table from the compressed file.
+	 */
 	while(c != '#'){
 
 		string code;
@@ -169,11 +202,18 @@ void huffman_c::uncompress_file (){
 	ofstream out_file;
 	out_file.open(uncomp_file, ios::out);
 
+	/**
+	 * Decode the encoded text using the above extracted encoding table.
+	 */
 	bool end=0;
 	while(file.get(c)){
 
 		p = c;
 
+		/**
+		 * Append one bit at a time and check whether a valid huffman encoding can
+		 * be found in the encoding table.
+		 */
 		for(int i=7; i>=0; i--){
 			
 			if((p>>i) & 1)
@@ -201,11 +241,17 @@ void huffman_c::uncompress_file (){
 	}
 }
 
+/**
+ * @brief Compress or Uncompress using constant length huffman encoding.
+ * 
+ * @param uncomp_file Path to uncompressed file.
+ * @param comp_file Path to compressed file.
+ * @param opt set opt to 1 to compress, 0 to uncompress
+ */
 huffman_c::huffman_c(string uncomp_file, string comp_file, bool opt){
 	this->uncomp_file = uncomp_file;
 	this->comp_file = comp_file;
 
-	// 1 to compress, 0 to uncompress
 	if(opt){
 		generate_encoding();
 		save_compressed_file();
